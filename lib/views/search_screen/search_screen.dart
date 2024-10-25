972d5ad4687d22c7188ea597cfc61499223ba9cc
Ethan-Olowo <Ethan.olowo@strathmore.edu>
2024-10-25 11:29:14 +0300

    Refactor SearchScreen to add search functionality

--- lib/views/search_screen/search_screen.dart
+++ lib/views/search_screen/search_screen.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
+
+  @override
+  SearchScreenState createState() => SearchScreenState();
+}
+
+class SearchScreenState extends State<SearchScreen> {
+  String searchText = "";
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> search() async {
+    setState(() {
+      // TODO: Implement search functionality
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-        // title: const Text('Search'),
-      // ),
-      body: const Center(
-        child: Text(
-          'Welcome Search or Should I say Sirch'
+      body: Center(
+          child: Column(children: [
+        TextField(
+          key: const Key('searchField'),
+          decoration: const InputDecoration(
+            labelText: 'Search',
+            prefixIcon: Icon(Icons.search),
           ),
-      ),
+          //Update _searchText on user input change
+          onChanged: (text) {
+            setState(() {
+              searchText = text;
+            });
+          },
+        ),
+        const Text('Welcome Search or Should I say Sirch'),
+      ])),
     );
   }
 }
+
