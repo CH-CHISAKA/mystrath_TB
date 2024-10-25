8093daf165034a2c37256a11a0935d7ab718389f
Ethan-Olowo <Ethan.olowo@strathmore.edu>
2024-10-25 10:50:04 +0300

    .gitignore is now working

--- .dart_tool/flutter_build/c1db361485babbb19ca69f18b9b07425/native_assets.d
+++
@@ -1,1 +0,0 @@
- /Users/mac/Desktop/STRATHMORE\ PROJECT/MyStrath/mystrath/.dart_tool/flutter_build/c1db361485babbb19ca69f18b9b07425/native_assets.yaml: 
--- android/app/src/main/kotlin/com/example/mystrath/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mystrath
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
